a53b3e0ad7a85041d239cc7b973c129094783f68
Levent Emre PAÇAL <v4inthehood@gmail.com>
2023-02-12 15:53:32 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,6 +6,8 @@
     
     if(getCookie("kizilaygosterme")==1) {
         document.querySelector(".kizilay-popup").style.display = "none";
+    } else {
+        document.querySelector(".kizilay-popup").style.display = "block";
     }
     
     document.querySelector(".kizilay-close").addEventListener("click", function() {
